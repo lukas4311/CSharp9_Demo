fd2a68dd1695779cf3fc6bc27ac2d8ae6309d5d1
Lukáš Salfický <salficky.job@gmail.com>
2020-11-14 11:31:19 +0100

    Project with pattern matching.

---
+++ ImprovedPatternMatching/DeliveryTruck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprovedPatternMatching
+{
+    public class DeliveryTruck
+    {
+        public int GrossWeightClass { get; set; }
+
+        public int Passangers { get; set; }
+
+        public void Deconstruct(out int grossWeightClass, out int passangers) => (grossWeightClass, passangers) = (this.GrossWeightClass, this.Passangers);
+    }
+}
---
+++ ImprovedPatternMatching/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ImprovedPatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeliveryTruck deliveryTruck = new DeliveryTruck() { GrossWeightClass = 8000, Passangers = 2 };
+
+            Console.WriteLine(CalculateToll(deliveryTruck));
+            //Console.WriteLine(CalculateToll2(deliveryTruck));
+        }
+
+        // Simple pattern
+        public static decimal CalculateToll(object vehicle) =>
+            vehicle switch
+            {
+                DeliveryTruck t when t.GrossWeightClass > 5000 => 10.00m + 5.00m,
+                DeliveryTruck t when t.GrossWeightClass < 3000 => 10.00m - 2.00m,
+                DeliveryTruck _ => 10.00m,
+                _ => throw new ArgumentException("Not a known vehicle type", nameof(vehicle))
+            };
+
+        // Relational pattern
+        public static decimal CalculateToll2(object vehicle) =>
+            vehicle switch
+            {
+                DeliveryTruck t => t.GrossWeightClass switch
+                {
+                    > 5000 => 10.00m + 5.00m,
+                    < 3000 => 10.00m - 2.00m,
+                    _ => 10.00m,
+                },
+
+                _ => throw new ArgumentException("Not a known vehicle type", nameof(vehicle))
+            };
+
+        // Logical pattern and keywork not
+        public static decimal CalculateToll3(object vehicle) =>
+            vehicle switch
+            {
+                DeliveryTruck t => t.GrossWeightClass switch
+                {
+                    > 5000 => 10.00m + 5.00m,
+                    > 3000 and < 5000 => 10.00m,
+                    < 3000 => 10.00m - 2.00m,
+                    _ => 20.00m,
+                },
+                not null => throw new ArgumentException($"Not a known vehicle type: {vehicle}", nameof(vehicle)),
+                _ => throw new ArgumentException("Not a known vehicle type", nameof(vehicle))
+            };
+    }
+}
