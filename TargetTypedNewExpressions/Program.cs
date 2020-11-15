f1da8f7e8b3cf37a128782dca38f8ccbc120c32d
Lukáš Salfický <salficky.job@gmail.com>
2020-11-15 16:37:51 +0100

    Used target type new expression.

---
+++ TargetTypedNewExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace TargetTypedNewExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new(3, 5);
+            Point[] ps = { new(1, 2), new(5, 2), new(5, -3), new(1, -3) };
+        }
+    }
+}
