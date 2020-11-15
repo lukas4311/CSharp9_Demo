93bac6a4aa1bd6a735b4b9c9090b3690e6cea503
Lukáš Salfický <salficky.job@gmail.com>
2020-11-15 16:50:26 +0100

    Used static function in lambda expression

---
+++ StaticAnonymousFunctions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StaticAnonymousFunctions
+{
+    class Program
+    {
+        const string message = "{0} is awaiable country to travel while COVID";
+
+        static void Main(string[] args)
+        {
+            PromoteCountry(static country => string.Format(message, country));
+        }
+
+        static void PromoteCountry(Func<string, string> func)
+        {
+            var countries = new List<string> { "Canada", "Russia", "Japan" };
+
+            foreach (var country in countries)
+                Console.WriteLine(func(country));
+        }
+    }
+}
