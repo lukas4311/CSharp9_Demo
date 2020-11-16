90429434197b865b20c7ea82590799b87aabf245
Lukáš Salfický <salficky.job@gmail.com>
2020-11-16 19:44:31 +0100

    Add type patterns in switch statement.

---
+++ TypePatterns/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TypePatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type patterns!");
+        }
+
+        void M(object o1, object o2)
+        {
+            var t = (o1, o2);
+
+            if (t is (int, string)) { 
+                // test zda proměnnám sedí datový typ
+            } 
+            
+            switch (o1)
+            {
+                case int: break; // test if o1 is an int
+                case System.String: break; // test if o1 is a string
+            }
+        }
+
+        private void M2(object o)
+        {
+            switch (o)
+            {
+                case int number:
+                    Console.WriteLine($"Number + 1 = {number + 1}");
+                    break;
+                case string text:
+                    Console.WriteLine($"Text + \"@\": {text + "@"}");
+                    break;
+            }
+        }
+    }
+}
