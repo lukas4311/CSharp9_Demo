75037f03205cca5b7e2f5a05f95963684d794a27
Lukáš Salfický <salficky.job@gmail.com>
2020-11-16 19:30:45 +0100

    Add usage of lambda discard

---
+++ LambdaDiscardParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaDiscardParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labda discard parameters!");
+
+            List<int> items = new List<int> { 1, 2, 3, 4, 5 };
+            items.ForEach(_ => Console.WriteLine("Proste něco!!"));
+        }
+    }
+}
