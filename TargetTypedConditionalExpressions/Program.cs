db029e2e42ad61c6d1526771b0cf5e6f5b141150
Lukáš Salfický <salficky.job@gmail.com>
2020-11-16 19:49:58 +0100

    Target type conditional expressions

---
+++ TargetTypedConditionalExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TargetTypedConditionalExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Target-typed conditional expressions!");
+
+            int? number = true ? 1 : null;
+        }
+    }
+}
